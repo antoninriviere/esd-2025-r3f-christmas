d95f4e43b9df3c221e89e7bee56336b4c0145a20
Antonin Riviere <antonin.rvr@gmail.com>
2025-04-01 11:28:22 +0200

    :sparkles: Presents > handle click

--- src/Presents.jsx
+++ src/Presents.jsx
@@ -6,19 +6,33 @@ export default function Presents() {
     const present2 = useGLTF('./models/present-2.glb')
     const present3 = useGLTF('./models/present-3.glb')
 
+
+    const onClickPresent = (index, event) => {
+        event.stopPropagation()
+        console.log('clicked present', index)
+    }
+
+
+
     return <>
         <RigidBody restitution={1}>
-            <Clone object={present1.scene} position={[3, 6, 1]} />
+            <Clone object={present1.scene} position={[3, 6, 1]} onClick={(event) => {
+                onClickPresent(1, event)
+            }} />
             {/* <CuboidCollider args={[0.5, 0.5, 0.5]} position={[0, -0.5, 0]} mass={1} /> */}
         </RigidBody>
 
         <RigidBody restitution={1}>
-            <Clone object={present2.scene} position={[0, 6, -4]} />
+            <Clone object={present2.scene} position={[0, 6, -4]} onClick={(event) => {
+                onClickPresent(2, event)
+            }} />
             {/* <CuboidCollider args={[0.5, 0.5, 0.5]} position={[0, -0.5, 0]} mass={1} /> */}
         </RigidBody>
 
         <RigidBody restitution={1}>
-            <Clone object={present3.scene} position={[-3, 6, -2]} />
+            <Clone object={present3.scene} position={[-3, 6, -2]} onClick={(event) => {
+                onClickPresent(3, event)
+            }} />
             {/* <CuboidCollider args={[0.5, 0.5, 0.5]} position={[0, -0.5, 0]} mass={1} /> */}
         </RigidBody>
     </>
